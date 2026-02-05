1f00d7b0277b4fb46c300c12e38a3199ec2eada1
mahanzavari <mahanzavari@gmail.com>
2026-02-05 20:52:44 +0330

    done task 2, 3

--- team7/urls.py
+++ team7/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.base),
-    path("ping/", views.ping),
+    # API Endpoints
+    path("api/submit-writing/", views.submit_writing, name="submit_writing"),
+    
+    # HTML Views (Front-end will use these later)
+    path("", views.base, name="team7_index"),
+    path("ping/", views.ping, name="team7_ping"),
 ]
